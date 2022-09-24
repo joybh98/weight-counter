2f9ddc037bea40b280ec2ace3ba85aafb5c488a1
Joy Bhalla <joybh@windows.localdomain>
2022-09-24 23:21:13 +0530

    added form skeleton

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ document.addEventListener("click",togglePopup);
 function togglePopup() {
     document.getElementById("popup-1").style.display="block";
 }
+
+document.addEventListener("submit",saveInput);
+
+function saveInput() {
+    var image=document.getElementById("picture").value;
+    var weight=document.getElementById("weight").value;
+    var date=document.getElementById("date").value;
+
+    console.log(weight,date);
+}
