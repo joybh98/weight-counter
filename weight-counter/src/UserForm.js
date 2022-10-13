cdd536599cf0e1e672060317e97338c11a9a3c7f
Joy Bhalla <joybh@windows.localdomain>
2022-10-13 11:55:17 +0530

    added cloudinary logic

--- weight-counter/src/UserForm.js
+++ weight-counter/src/UserForm.js
@@ -1,24 +1,34 @@
 import './UserForm.css';
+import React, { useState } from "react";
 
 function UserForm() {
-    let userInfo= {
-        image: '',
-        weight: 0,
-        date: ''
-    }
 
-    //get user info
-    const getUserInfo = (event) => {
-        //prevent initial loading
+    //add state to store name and file of data
+    const[inputImage,setImage]=useState(null);
+    const handleInput = event => {
+        //prevent default form behaviour
         event.preventDefault();
-        alert('User info was gotten');
+        const formData=new FormData();
+        formData.append("file",inputImage);
+
+        fetch("https://api.cloudinary.com/v1_1/dpzbix45x/upload",{
+            method:'POST',
+            body:formData
+        }).then(response=>response.json())
+        .then(data=>{
+            if (data.secure_url !== '') {
+                this.setState({
+                  uploadedFileCloudinaryUrl: data.secure_url
+                });
+            }
+        }).catch(err=>console.error(err))
     }
 
     return(
         <div className="userInfo">
-            <form onSubmit={getUserInfo}>
-                <label>Image</label>
-                <input type="file"/>
+            <form onSubmit={handleInput}>
+                <label>Upload Image</label>
+                <input type="file" value={inputImage} onChange={(e)=>setImage(e.target.files[0])}/>
             </form>
         </div>
     )
