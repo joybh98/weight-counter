8171ce30a8aaf5a18ca155c36bf17d94c3c782e5
Joy Bhalla <joybh@windows.localdomain>
2022-10-14 10:04:53 +0530

    added basic form

--- weight-counter/src/UserForm.js
+++ weight-counter/src/UserForm.js
@@ -3,33 +3,21 @@ import React, { useState } from "react";
 
 function UserForm() {
 
-    //add state to store name and file of data
-    const[inputImage,setImage]=useState(null);
-    const handleInput = event => {
-        //prevent default form behaviour
-        event.preventDefault();
-        const formData=new FormData();
-        formData.append("file",inputImage);
+    const [image,setImage]=useState('');
 
-        fetch("https://api.cloudinary.com/v1_1/dpzbix45x/upload",{
-            method:'POST',
-            body:formData
-        }).then(response=>response.json())
-        .then(data=>{
-            if (data.secure_url !== '') {
-                this.setState({
-                  uploadedFileCloudinaryUrl: data.secure_url
-                });
-            }
-        }).catch(err=>console.error(err))
+    const handleFile = (e) => {
+        console.log(e.target.files[0]);
+        setImage(e.target.files[0]);
     }
 
+    const submitImage=()=>{
+        //call api
+    }
     return(
         <div className="userInfo">
-            <form onSubmit={handleInput}>
-                <label>Upload Image</label>
-                <input type="file" value={inputImage} onChange={(e)=>setImage(e.target.files[0])}/>
-            </form>
+            <label>Select File</label>
+            <input type="file" name="file" onChange={(e)=>handleFile(e)}/>
+            <button onClick={submitImage()}>Upload</button>
         </div>
     )
 }
